34649e85d945bfe65c3cb8a37e4f5e0a39f579be
Simon Branford <s.j.branford@bham.ac.uk>
2020-10-30 21:14:29 +0000

    lib64 / lib patch

--- easybuild/tools/config.py
+++ easybuild/tools/config.py
@@ -267,6 +267,7 @@ BUILD_OPTIONS_CMDLINE = {
         'cleanup_tmpdir',
         'extended_dry_run_ignore_errors',
         'fixed_installdir_naming_scheme',
+        'lib64_lib_symlink',
         'mpi_tests',
         'map_toolchains',
         'modules_tool_version_check',
